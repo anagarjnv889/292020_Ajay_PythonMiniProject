deb5e429d5a23503ae5ea05e2ebad5c447ff0acc
Ajay Kumar Nagar <anagarjnv889@gmail.com>
2021-04-27 16:54:12 +0530

    292020 update

--- project1.py
+++ project1.py
@@ -2,6 +2,13 @@ print("___________________________________\n")
 print("Welcome to Python Calculator")
 print("___________________________________\n")
 
+def addition():
+    
+def subtraction():
+def multiplication():
+def division():
+def modulus():
+
 def arithmetic_operation():
     print("____________________________________")
     print("\nWelcome to Arithmetic Calculator")
@@ -12,10 +19,21 @@ def arithmetic_operation():
     print("Press 4 for Division................")
     print("Press 5 for Modulus.................\n")
     n=int(input("Enter your choice for Arithmetic Operation\n"))
-    
+    if n==1:
+        print(addition())
+    elif n==2:
+        print(subtraction())
+    elif n==3:
+        print(multiplication())
+    elif n==4:
+        print(division())
+    elif n==5:
+        print(modulus())
+    else:
+        print("Enter a valid input\n")
     
 def advance_operation():
-    print("Press 6 for HCF")
+    print("Press 1 for HCF")
     print("Press 1 for addition")
     print("Press 1 for addition")
 
